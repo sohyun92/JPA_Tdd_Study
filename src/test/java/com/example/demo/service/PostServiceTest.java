c5f29aea1f3527ae80898816dca02bff33fadcc3
0830thgus@naver.com <0830thgus@naver.com>
2024-11-17 19:31:35 +0900

    postervice 메소드에 테스트 코드 추가

---
+++ src/test/java/com/example/demo/service/PostServiceTest.java
@@ -0,0 +1,80 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.CertificationCodeNotMatchedException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.PostCreateDto;
+import com.example.demo.model.dto.PostUpdateDto;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.example.demo.repository.PostEntity;
+import com.example.demo.repository.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+
+
+@SpringBootTest
+@TestPropertySource("classpath:test-application.properties")
+@SqlGroup({
+        @Sql(value = "/sql/post-service-test-data.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+
+})
+public class PostServiceTest {
+    @Autowired
+    private PostService postService;
+
+    @Test
+    void getById는_존재하는_게시물을_내려준다(){
+        PostEntity result = postService.getById(1);
+        assertThat(result.getContent()).isEqualTo("helloworld");
+        assertThat(result.getWriter().getEmail()).isEqualTo("0830thgus@naver.com");
+    }
+
+    @Test
+    void postCreateDto_를_이용하여_게시물을_생성할_수_있다() {
+        // given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1)
+                .content("foobar")
+                .build();
+
+        // when
+        PostEntity result = postService.create(postCreateDto);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getContent()).isEqualTo("foobar");
+        assertThat(result.getCreatedAt()).isGreaterThan(0);
+    }
+
+    @Test
+    void postUpdateDto_를_이용하여_게시물을_수정할_수_있다() {
+        // given
+        PostUpdateDto postUpdateDto = PostUpdateDto.builder()
+                .content("hello world :)")
+                .build();
+
+        // when
+        postService.update(1, postUpdateDto);
+
+        // then
+        PostEntity postEntity= postService.getById(1);
+        assertThat(postEntity.getContent()).isEqualTo("hello world :)");
+        assertThat(postEntity.getModifiedAt()).isGreaterThan(0);
+    }
+
+
+}
---
+++ src/test/resources/sql/post-service-test-data.sql
@@ -0,0 +1,6 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, '0830thgus@naver.com', 'so', 'Seoul', 'aaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, '08302thgus@naver.com', 'so2', 'Seoul', 'aaaaaaaaaaab', 'PENDING', 0);
+insert into `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`)
+values (1, 'helloworld', 1678530673958, 0, 1);
